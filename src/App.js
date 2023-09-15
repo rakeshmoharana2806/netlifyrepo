fee0f71f2ec46738ab4298d6658eb14fa64e3720
rakeshmoharana2806 <send.lipun@gmail.com>
2023-09-15 10:28:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        This is my first netlify application
         </a>
       </header>
     </div>
